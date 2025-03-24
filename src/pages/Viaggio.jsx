ffa25a1bd2637e3e013302ea8d528bce6d930c6a
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-24 11:50:07 +0100

    add: form ricerca

--- src/pages/Viaggio.jsx
+++ src/pages/Viaggio.jsx
@@ -15,6 +15,14 @@ export default function Viaggio() {
     <>
       <main>
         <h1>Viaggio 1</h1>
+        {/* form ricerca */}
+        <div>
+          <form>
+            <label htmlFor="ricercaViaggiatore">Cerca viaggiatore per cognome</label>
+            <input name="ricercaViaggiatore" type="text" placeholder="cognome viaggiatore" />
+            <button type="submit">Cerca</button>
+          </form>
+        </div>
         <div>
           <ul>
             {arrayViaggiatori.map((viaggiatore) => {
