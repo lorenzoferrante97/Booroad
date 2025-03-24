603e6fab58fabfa6d55d3bf0114f3e56d16cae76
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-03-24 10:26:28 +0100

    add array viaggi

---
+++ src/data/db.js
@@ -0,0 +1,28 @@
+const viaggi = [
+    {
+      id: 1,
+      nome: "Vacanza estiva in Italia",
+      luogo: "Roma",
+      data: "2025-06-15"
+    },
+    {
+      id: 2,
+      nome: "Escursione in montagna",
+      luogo: "Alpi",
+      data: "2025-08-10"
+    },
+    {
+      id: 3,
+      nome: "Tour della Spagna",
+      luogo: "Barcellona",
+      data: "2025-07-20"
+    },
+    {
+      id: 4,
+      nome: "Viaggio a Tokyo",
+      luogo: "Tokyo",
+      data: "2025-09-05"
+    }
+  ];
+
+  
